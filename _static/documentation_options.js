b25df938c7e389b5fb53037e0a9a1a75be5f70d5
CMeza99 <CMeza99@users.noreply.github.com>
2020-08-29 07:31:30 +0000

    deploy: 8f04de38373aa6a0ba77a0fcd9171b4a002b7341

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.1.dev1+g3c227ac',
+    VERSION: '0.1.dev1+g8f04de3',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
